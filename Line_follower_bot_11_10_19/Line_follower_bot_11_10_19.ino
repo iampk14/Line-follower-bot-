c56899e86f03caff28302aa27f10fe658863e4e8
iampk14 <54847707+iampk14@users.noreply.github.com>
2019-10-11 23:07:10 +0530

    Update Line_follower_bot_11_10_19.ino

--- Line_follower_bot_11_10_19/Line_follower_bot_11_10_19.ino
+++ Line_follower_bot_11_10_19/Line_follower_bot_11_10_19.ino
@@ -1,51 +1,41 @@
-int forwardRight = 12;   //right motor, forward action
-int forwardLeft = 9;    //left motor, forward action
-int backwardRight = 10;   //right motor, backward action
-int backwardLeft = 8;    //left motor, backward action
+const int forwardRight = 12;   //right motor, forward action
+const int forwardLeft = 9;    //left motor, forward action
 
 const int IrSensorR=A5;   //Ir sensor on right side
 const int IrSensorL=A3;   //Ir sensor on left side
+
 int IrReadingR, IrReadingL ;
+
 void setup()
 {
   pinMode(IrSensorR,INPUT);
   pinMode(IrSensorL,INPUT);
   pinMode(forwardRight, OUTPUT);
   pinMode(forwardLeft, OUTPUT);
-  pinMode(backwardRight, OUTPUT);
-  pinMode(backwardLeft, OUTPUT);
   Serial.begin(9600);  
 }
 
-void FR()   //function for forward action of the right motor
+void Forward()   //function for forward action
 {
   digitalWrite(forwardRight, HIGH);
-  digitalWrite(backwardRight, LOW);
-}
-
-void FL()   //function for forward action of the left motor
-{
   digitalWrite(forwardLeft, HIGH);
-  digitalWrite(backwardLeft, LOW);
 }
 
 void right()   //function for right action of the robot
 {
   digitalWrite(forwardLeft, HIGH);
-  digitalWrite(backwardRight, LOW);
+  digitalWrite(forwardRight, LOW);
 }
 
 void left()    //function for left action of the robot
 {
   digitalWrite(forwardRight, HIGH);
-  digitalWrite(backwardLeft, LOW);
+  digitalWrite(forwardLeft, LOW);
 }
 
 void allstop()    //function for stopping the robot
 {
   digitalWrite(forwardRight, LOW);
-  digitalWrite(backwardRight, LOW);
-  digitalWrite(backwardLeft, LOW);
   digitalWrite(forwardLeft, LOW);
 }
 
@@ -71,8 +61,7 @@ void loop()
   }
   if(IrReadingR==1 && IrReadingL==1)                     //Condition for the bot to move forward 
   {
-    FR();
-    FL();
+    Forward();
   }
   else if(IrReadingR==1 && IrReadingL==0)                //Condition for the bot to move right
   {
